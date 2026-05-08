e53a2f32430bf326581a712484e8318bc5edc0eb
yassine <yassinesaidani757@gmail.com>
2026-05-08 07:32:08 +0100

    changes

--- connect.php
+++ connect.php
@@ -1,24 +1,12 @@
 <?php
-// db_connect.php - PURE Procedural
+// connect.php - Pure connection only
 $host = 'localhost';
 $user = 'root';
 $password = '';
 $dbname = 'Shop';
-
-
 $conn = mysqli_connect($host, $user, $password, $dbname);
-
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-
 mysqli_set_charset($conn, "utf8");
-
-
-$sql = "SELECT * FROM users";
-$result = mysqli_query($conn, $sql);
-
-
-
 ?>
